efab3b73ffb5b52c4d372894622739d55505ec30
Lenard <lenardcortuna05@gmail.com>
2026-02-03 13:44:52 +0800

    Updated AppConstant.

---
+++ Constants/AppConstant.swift
@@ -0,0 +1,48 @@
+//
+//  AppConstant.swift
+//  Cartoonify
+//
+//  Created by Lenard Cortuna on 1/13/26.
+//
+
+class AppConstant {
+    
+    struct LoginScreen {
+        static let firstNamePlaceholder = "First Name"
+        static let lastNamePlaceholder = "Last Name"
+        static let loginScreenTitle = "Cartoonify"
+        static let loginTitle = "Login"
+        static let logoImageName = "cartoonify_logo"
+        static let emailPlaceholder = "Email"
+        static let passwordPlaceholder = "Password"
+        static let loginButtonTitle = "Login"
+        static let showPasswordImageName = "eye.fill"
+        static let hidePasswordImageName = "eye.slash.fill"
+        static let registerButtonTitle = "Register"
+        static let dontHaveAccountText = "Don't have an account?"
+        static let alreadyHaveAccountText = "Already have an account?"
+        static let orText = "or"
+        static let appleImageName = "apple_button"
+        static let googleImageName = "google_button"
+    }
+    
+    struct HomeScreen {
+        static let searchPlaceholder = "Search"
+        static let searchImageName = "search_icon"
+        static let cnFavoritesText = "CN Favorites"
+        static let seeAllText = "See All"
+        static let popularButtonText = "Popular"
+        static let classicButtonText = "Classic"
+        static let kidsFavoritesButtonText = "Kids Favorites"
+    }
+    
+    struct SupabaseClient {
+        static let supabaseURL = "https://puwyskgxbnhrnikoewaz.supabase.co"
+        static let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1d3lza2d4Ym5ocm5pa29ld2F6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyMDM2OTksImV4cCI6MjA4NDc3OTY5OX0.o37VHxawB04IMLEfw4WGOS8rKhW9daja2AO7YgZdeXU"
+    }
+    
+    struct FavoriteScreen {
+        static let myFavoritesText = "My Favorites"
+    }
+    
+}
