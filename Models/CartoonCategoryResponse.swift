58a4e08d10885f1984a439cb43fa62c6f53c553f
Lenard <lenardcortuna05@gmail.com>
2026-02-04 08:46:10 +0800

    CartoonCategoryResponse

---
+++ Models/CartoonCategoryResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CartoonCategoryResponse.swift
+//  Cartoonify
+//
+//  Created by Lenard Cortuna on 1/27/26.
+//
+
+import SwiftUI
+
+struct CartoonCategoryResponse: Decodable {
+    let cartoons: Cartoon
+}
+
