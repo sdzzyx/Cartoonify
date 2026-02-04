288ad606a53cc5b61d1ff98e5a0a16e8a37738ab
Lenard <lenardcortuna05@gmail.com>
2026-02-04 08:47:01 +0800

    HomeTopView

--- Views/Home/ReusableView/HomeTopView.swift
+++ Views/Home/ReusableView/HomeTopView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct HomeTopView: View {
+    //@StateObject private var viewModel = HomeViewModel()
+    @Binding var searchText: String
+    let isSearchFocused: FocusState<Bool>.Binding
+    let allCartoons: [Cartoon]
+    @Binding var categoryCartoons: [Cartoon]
+    @Binding var selectedCategory: CartoonCategory
+    
     var body: some View {
-        VStack(alignment: .center) {
-            Image(AppConstant.LoginScreen.logoImageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 220)
+        VStack(alignment: .leading, spacing: 12) {
+            
+            // Logo
+            HStack {
+                Spacer()
+                Image(AppConstant.LoginScreen.logoImageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 220)
+                Spacer()
+            }
+            
+            // Search
+            SearchTextField(
+                text: $searchText,
+                isFocused: isSearchFocused
+            )
+            .padding(.horizontal)
+            
+            // Section header
+            SectionHeaderView(
+                title: AppConstant.HomeScreen.cnFavoritesText,
+                actionText: AppConstant.HomeScreen.seeAllText
+            ) {
+                print("See All tapped")
+            }
+            .padding(.horizontal)
+            
+            // Horizontal Image list
+            HorizontalCartoonList(cartoons: allCartoons)
+            
+            // Category Buttons
+            CategorySelectorView(
+                selectedCategory: $selectedCategory
+            )
+            
+            // Category results
+            VerticalCartoonList(cartoons: $categoryCartoons)
             
         }
-        .paddin/g(.top, 150)
+        .padding(.top, 20)
     }
 }
