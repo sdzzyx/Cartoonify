322033afbe4abcea6787f9027eceec6094069ec2
Lenard <lenardcortuna05@gmail.com>
2026-02-04 08:47:57 +0800

    SectionHeaderView

--- Views/Home/ReusableView/SectionHeaderView.swift
+++ Views/Home/ReusableView/SectionHeaderView.swift
@@ -1,7 +1,32 @@
-ct//
-//  Se.swift
+//
+//  SectionHeaderView.swift
 //  Cartoonify
 //
 //  Created by Lenard Cortuna on 1/26/26.
 //
+
+import SwiftUI
+
+struct SectionHeaderView: View {
+    let title: String
+    let actionText: String
+    let action: () -> Void
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.system(size: 15, weight: .bold))
+                .foregroundColor(.primary)
+            
+            Spacer() // pushes See All to the right
+            
+            Button(action: action) {
+                Text(actionText)
+                    .font(.system(size: 15, weight: .medium))
+                    .foregroundColor(.blue)
+            }
+        }
+        .padding(.leading, 5) 
+    }
+}
 
