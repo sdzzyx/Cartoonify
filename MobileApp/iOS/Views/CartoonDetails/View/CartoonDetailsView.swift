ef88c579a73d6b89e68521dc7e9eeb607a715458
Lenard <lenardcortuna05@gmail.com>
2026-02-04 17:10:56 +0800

    Added CartoonDetailsView

---
+++ MobileApp/iOS/Views/CartoonDetails/View/CartoonDetailsView.swift
@@ -0,0 +1,14 @@
+//
+//  CartoonDetailsView.swift
+//  Cartoonify
+//
+//  Created by Lenard Cortuna on 2/4/26.
+//
+
+import SwiftUI
+
+struct CartoonDetailsView: View {
+    var body: some View {
+        Text("Cartoon Details")
+    }
+}
